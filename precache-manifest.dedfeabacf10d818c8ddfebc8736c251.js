15880eccaef58c42efb6e9c8713bbe712f23cf42
Mikhail Korovai <korovai.personal@gmail.com>
2020-04-16 23:03:38 +0300

    Updates

---
+++ precache-manifest.dedfeabacf10d818c8ddfebc8736c251.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80fa4ab2be6395fc3c73429a31c67d2",
+    "url": "/book-store/index.html"
+  },
+  {
+    "revision": "526ba87e961c1800bde5",
+    "url": "/book-store/static/css/main.081bf4b6.chunk.css"
+  },
+  {
+    "revision": "8755b3d2ce36de40605d",
+    "url": "/book-store/static/js/2.23044eed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/book-store/static/js/2.23044eed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "526ba87e961c1800bde5",
+    "url": "/book-store/static/js/main.df06672c.chunk.js"
+  },
+  {
+    "revision": "1fedc942ab78878a130f",
+    "url": "/book-store/static/js/runtime-main.985857a3.js"
+  }
+]);
